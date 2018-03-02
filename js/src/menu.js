9442fd19d037561591f6dc4cc19c2f9405dee0bf
Cosmaty1991 <cosmaty1991@gmail.com>
2018-03-02 17:31:55 +0200

    Initial commit

--- js/src/menu.js
+++ js/src/menu.js
@@ -1,15 +1,20 @@
 // menu for mobile and tablet
 
+var modalButton = document.querySelector('.header__modal-button');
+var modalWindow = document.querySelector('.header__modal-window');
+var modalItem = document.querySelectorAll('.header__modal-window .header__item');
+var modalClose = document.querySelector('.header__modal-close');
+
 // click on the menu button to show the modal window
 
-document.querySelector('.header__modal-button').onclick = function() {
-  document.querySelector('.header__modal-window').style.display = 'block';
+modalButton.onclick = function() {
+  modalWindow.style.display = 'block';
 }
 
 // click on the menu item to show or close the sub-menu
 
-for (var i = 0; i < document.querySelectorAll('.header__modal-window .header__item').length; i++) {
-  document.querySelectorAll('.header__modal-window .header__item')[i].onclick = function() {
+for (var i = 0; i < modalItem.length; i++) {
+  modalItem[i].onclick = function() {
     if (this.querySelector('.header__sublist').style.display == 'block') {
       this.querySelector('.header__sublist').style.display = 'none';
     } else {
@@ -20,6 +25,6 @@ for (var i = 0; i < document.querySelectorAll('.header__modal-window .header__it
 
 // click on the close button to close the modal window
 
-document.querySelector('.header__modal-close').onclick = function() {
-  document.querySelector('.header__modal-window').style.display = 'none';
+modalClose.onclick = function() {
+  modalWindow.style.display = 'none';
 }
